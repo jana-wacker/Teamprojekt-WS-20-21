226ad162f6e08640504f10a464791bae1e562483
NoraGit8 <73950217+NoraGit8@users.noreply.github.com>
2020-12-01 10:57:13 +0100

    Crawler with different options

--- teamproject/crawler.py
+++ teamproject/crawler.py
@@ -30,8 +30,17 @@ import json
 
 def fetch_data():
     year = 2003
+
+    # There's an array for every information we want to save in a separate file
+    team1 = []
+    team1points = []
+    team2 = []
+    team2points = []
+    location = []
+    date = []
+
     # Two while loops to go through the different years and gamedays
-    while year < 2021:
+    while year < 2005:
         year = year + 1
         gameday = 1
 
@@ -41,13 +50,7 @@ def fetch_data():
             r = requests.get('https://www.openligadb.de/api/getmatchdata/bl1/' + str(year) + '/' + str(gameday))
             r_dict = r.json()
 
-            # There's an array for every information we want to save in a separate file
-            team1 = []
-            team1points = []
-            team2 = []
-            team2points = []
-            location = []
-            date = []
+
 
             # The loop goes through all the information in the dictionary and adds the information to the right array
             for i in r_dict:
@@ -64,12 +67,7 @@ def fetch_data():
 
                 else:
                     continue
-            print(team1)
-            print(team2)
-            print(team1points)
-            print(team2points)
-            print(location)
-            print(date)
+
 
 
             # Creating a dictionary with all the relevant information
@@ -90,3 +88,61 @@ def fetch_data():
 
 
 fetch_data()
+
+
+def fetch_all_data():
+    year = 2004
+    counter = 1
+
+    # There's an array for every information we want to save in a separate file
+    team1 = []
+    team1points = []
+    team2 = []
+    team2points = []
+    location = []
+    date = []
+
+    while year < 2021:
+        r = requests.get('https://www.openligadb.de/api/getmatchdata/bl1/' + str(year))
+        r_dict = r.json()
+
+
+
+        # The loop goes through all the information in the dictionary and adds the information to the right array
+        for i in r_dict:
+            if i['MatchIsFinished']:
+                team1.append(i['Team1']['TeamName'])
+                team2.append(i['Team2']['TeamName'])
+                team1points.append(i['MatchResults'][0]['PointsTeam1'])
+                team2points.append(i['MatchResults'][0]['PointsTeam2'])
+                date.append(i['MatchDateTimeUTC'])
+                if i['Location'] is None:
+                    location.append('Unbekannt')
+                else:
+                    location.append(i['Location']['LocationCity'])
+
+            else:
+                continue
+
+            # Creating a dictionary with all the relevant information
+            data = {
+                'Location': location,
+                'Date': date,
+                'Team1': team1,
+                'GoalsTeam1': team1points,
+                'GoalsTeam2': team2points,
+                'Team2': team2
+            }
+
+        # Creating a CSV file to store the information
+        df = pd.DataFrame(data)
+        df.to_csv('Alldata.csv', index=False)
+
+
+        year = year + 1
+        counter = counter + 1
+
+
+
+
+fetch_all_data()
