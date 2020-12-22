80aa612145a8d721a815b9ce0ab306f02574fb8a
yupei-git <yupei.yang@student.uni-tuebingen.de>
2020-12-22 10:25:01 +0100

    The test for Minimaler Vorhersage-Algorithmus is changed because of the file path changed.

--- tests/test_algo.py
+++ tests/test_algo.py
@@ -1,27 +1,28 @@
 from unittest import TestCase
-from teamproject.vorhersage_algo import data
+from teamproject.Algorithms.MinimalerVorhersageAlgo import data
+
 
 class Test(TestCase):
     """function: try to test the machtNumber method in vorhersage_algo, to make sure that
                  the result of match is non-negative."""
     def test_match_number(self):
-        from teamproject.vorhersage_algo import matchNumber
+        from teamproject.Algorithms.MinimalerVorhersageAlgo import matchNumber
         assert (matchNumber(data) >= 0)
 
     """function: try to test the ProHomeWin method in vorhersage_algo, to make sure that
                  the result of procentage is non-negative."""
     def test_pro_home_win(self):
-        from teamproject.vorhersage_algo import ProHomeWin
+        from teamproject.Algorithms.MinimalerVorhersageAlgo import ProHomeWin
         assert (ProHomeWin(data) >= 0)
 
     """function: try to test the ProHomeLoss method in vorhersage_algo, to make sure that
                  the result of procentage is non-negative."""
     def test_pro_home_loss(self):
-        from teamproject.vorhersage_algo import ProHomeLoss
+        from teamproject.Algorithms.MinimalerVorhersageAlgo import ProHomeLoss
         assert (ProHomeLoss(data) >= 0)
 
     """function: try to test the ProHomeTied method in vorhersage_algo, to make sure that
                  the result of procentage is non-negative."""
     def test_pro_home_tied(self):
-        from teamproject.vorhersage_algo import ProHomeTied
+        from teamproject.Algorithms.MinimalerVorhersageAlgo import ProHomeTied
         assert (ProHomeTied(data) >= 0)
