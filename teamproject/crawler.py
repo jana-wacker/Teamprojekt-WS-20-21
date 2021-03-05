fcd9c2e89f2e20958a82b1e23b41068befd2ef6b
NoraGit8 <73950217+NoraGit8@users.noreply.github.com>
2021-03-05 12:10:26 +0100

    Added comments in crawler

--- teamproject/crawler.py
+++ teamproject/crawler.py
@@ -11,17 +11,21 @@ from tkinter.messagebox import showinfo
 from datetime import datetime
 
 def fetch_matchday():
-    """Query data of the next match day and return in our internal format."""
+    """
+    Query data of the next match day and return in our internal format.
+    """
     gameday = 1
     team1 = []
     team2 = []
     nextMatches = []
     match_nr = []
 
+    # Loop to go through the gamedays
     while gameday <= 34:
         r = requests.get('https://www.openligadb.de/api/getmatchdata/bl1/' + (str(datetime.now().year-1)) + '/' + str(gameday))
         r_dict = r.json()
 
+        # The loop goes through all the information in the dictionary and adds the information to the right array
         for i in r_dict:
             if (i['MatchIsFinished'] == False):
                 team1.append(i['Team1']['TeamName'])
@@ -31,6 +35,7 @@ def fetch_matchday():
 
         gameday = gameday +1
 
+    # Creating a dictionary with all the relevant information
     data = {
         'Matchday': nextMatches,
         'Team1': team1,
@@ -247,11 +252,11 @@ def fetch_all_data():
             if i['MatchIsFinished']:
                 team1.append(i['Team1']['TeamName'])
                 team2.append(i['Team2']['TeamName'])
-                if (i['MatchResults'][0]['ResultDescription']) == str('Ergebnis nach Ende der offiziellen Spielzeit'):
+                if i['MatchResults'][0]['ResultDescription'] == str('Ergebnis nach Ende der offiziellen Spielzeit'):
                     team1points.append(i['MatchResults'][0]['PointsTeam1'])
                 else:
                     team1points.append(i['MatchResults'][1]['PointsTeam1'])
-                if (i['MatchResults'][0]['ResultDescription']) == str('Ergebnis nach Ende der offiziellen Spielzeit'):
+                if i['MatchResults'][0]['ResultDescription'] == str('Ergebnis nach Ende der offiziellen Spielzeit'):
                     team2points.append(i['MatchResults'][0]['PointsTeam2'])
                 else:
                     team2points.append(i['MatchResults'][1]['PointsTeam2'])
